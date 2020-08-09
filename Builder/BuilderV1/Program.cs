895cb1c47ea7fefc2d33441f19101e96197bc412
Wenchin <wenchin.ch@gmail.com>
2020-08-09 13:59:28 +0800

    Added Builder pattern

---
+++ Builder/BuilderV1/Program.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BuilderV1
+{
+
+    public interface ITripBuilder
+    {
+        void Reset();
+        void SetDestination(string destination);
+        void SetPrice();
+        void SetDifficulty();
+        void SetDurationHours();
+        void SetMaxParticipants();
+        void SetDescription();
+        void SetSalesContext();
+        Trip GetTrip();
+    }
+
+    public class SupTripBuilder : ITripBuilder
+    {
+        private Trip _trip;
+        private int _price = 4000;
+        private int _difficulty = 3;
+        private int _durationHours = 5;
+        private int _maxParticipants = 10;
+        private string _description = "站著、坐著、趴著都能玩的立槳衝浪";
+        private string _note = "9/1 - 9/31 SUP 行程打卡立享 9 折";
+        public SupTripBuilder()
+        {
+            Reset();
+        }
+        public void Reset()
+        {
+            _trip = new Trip();
+        }
+        public void SetDestination(string destination)
+        {
+            _trip.AddDetail($"地點: {destination}");
+        }
+        public void SetPrice()
+        {
+            _trip.AddDetail($"每人價格: NTD {_price}");
+        }
+        public void SetDifficulty()
+        {
+            _trip.AddDetail($"困難度: {_difficulty}/5");
+        }
+        public void SetDurationHours()
+        {
+            _trip.AddDetail($"時間: {_durationHours} 小時");
+        }
+        public void SetMaxParticipants()
+        {
+            _trip.AddDetail($"每團人數限制: {_maxParticipants} 人");
+        }
+        public void SetDescription()
+        {
+            _trip.AddDetail($"SUP 活動敘述: {_description}");
+        }
+        public void SetSalesContext()
+        {
+            _trip.AddDetail($"【{_note}】");
+        }
+        public Trip GetTrip()
+        {
+            return _trip;
+        }
+    }
+
+    public class KayakTripBuilder : ITripBuilder
+    {
+        private Trip _trip;
+        private int _price = 1500;
+        private int _difficulty = 2;
+        private int _durationHours = 4;
+        private int _maxParticipants = 20;
+        private string _description = "到海上划獨木舟";
+        private string _note = "使用振興券參加獨木舟行程 再折 100 元";
+        public KayakTripBuilder()
+        {
+            Reset();
+        }
+        public void Reset()
+        {
+            _trip = new Trip();
+        }
+        public void SetDestination(string destination)
+        {
+            _trip.AddDetail($"地點: {destination}");
+        }
+        public void SetPrice()
+        {
+            _trip.AddDetail($"每人價格: NTD {_price}");
+        }
+        public void SetDifficulty()
+        {
+            _trip.AddDetail($"困難度: {_difficulty}/5");
+        }
+        public void SetDurationHours()
+        {
+            _trip.AddDetail($"時間: {_durationHours} 小時");
+        }
+        public void SetMaxParticipants()
+        {
+            _trip.AddDetail($"每團人數限制: {_maxParticipants} 人");
+        }
+        public void SetDescription()
+        {
+            _trip.AddDetail($"SUP 活動敘述: {_description}");
+        }
+        public void SetSalesContext()
+        {
+            _trip.AddDetail($"【{_note}】");
+        }
+        public Trip GetTrip()
+        {
+            return _trip;
+        }
+    }
+
+    public class TripDirector
+    {
+        public Trip CreateTrip(ITripBuilder tripBuilder, string destination)
+        {
+            tripBuilder.SetSalesContext();
+            tripBuilder.SetDestination(destination);
+            tripBuilder.SetPrice();
+            tripBuilder.SetDifficulty();
+            tripBuilder.SetDurationHours();
+            tripBuilder.SetMaxParticipants();
+            tripBuilder.SetDescription();
+            return tripBuilder.GetTrip();
+        }
+    }
+
+    public class Trip
+    {
+        private IList<string> _tripDetail = new List<string>();
+        public void AddDetail(string detail)
+        {
+            _tripDetail.Add(detail);
+        }
+        public IEnumerable<string> GetDetail()
+        {
+            foreach (var item in _tripDetail)
+            {
+                yield return item;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void PrintTripDetail(IEnumerable<object> tripDetail)
+        {
+            foreach (var item in tripDetail)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("---");
+        }
+        static void Main(string[] args)
+        {
+            var tripDirector = new TripDirector();
+            Console.WriteLine("Trip 1. SUP 象鼻岩行程");
+            var trip = tripDirector.CreateTrip(new SupTripBuilder(), "深澳象鼻岩");
+            PrintTripDetail(trip.GetDetail());
+
+            Console.WriteLine("Trip 2. SUP 龜山島行程");
+            var trip1 = tripDirector.CreateTrip(new SupTripBuilder(), "龜山島牛奶湖");
+            PrintTripDetail(trip1.GetDetail());
+
+            Console.WriteLine("Trip 3. 獨木舟 東澳行程");
+            var trip2 = tripDirector.CreateTrip(new SupTripBuilder(), "東澳海蝕洞");
+            PrintTripDetail(trip2.GetDetail());
+
+            Console.WriteLine("Trip 4. 獨木舟 龍洞行程");
+            var trip3 = tripDirector.CreateTrip(new KayakTripBuilder(), "東北角龍洞灣");
+            PrintTripDetail(trip3.GetDetail());
+        }
+    }
+}
---
+++ Builder/BuilderV2/Program.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BuilderV2
+{
+    public interface ITripBuilder
+    {
+        ITripBuilder SetDestination(string destination);
+        ITripBuilder SetPrice(int price);
+        ITripBuilder SetDifficulty(int difficulty);
+        ITripBuilder SetDurationHours(int hours);
+        ITripBuilder SetMaxParticipants(int maxParticipants);
+        ITripBuilder SetDescription();
+        ITripBuilder SetSalesContext();
+        Trip Build();
+    }
+
+    public class SupTripBuilder : ITripBuilder
+    {
+        private Trip _trip;
+        private string _description = "站著、坐著、趴著都能玩的立槳衝浪";
+        private string _salesContext = "9/1 - 9/31 SUP 行程打卡立享 9 折";
+        public SupTripBuilder()
+        {
+            _trip = new Trip();
+        }
+        public ITripBuilder SetDestination(string destination)
+        {
+            _trip.SetDestination(destination);
+            return this;
+        }
+        public ITripBuilder SetPrice(int price)
+        {
+            _trip.SetPrice(price);
+            return this;
+        }
+        public ITripBuilder SetDifficulty(int difficulty)
+        {
+            _trip.SetDifficulty(difficulty);
+            return this;
+        }
+        public ITripBuilder SetDurationHours(int hours)
+        {
+            _trip.SetDurationHours(hours);
+            return this;
+        }
+        public ITripBuilder SetMaxParticipants(int maxParticipants)
+        {
+            _trip.SetMaxParticipants(maxParticipants);
+            return this;
+        }
+        public ITripBuilder SetDescription()
+        {
+            _trip.SetDescription(_description);
+            return this;
+        }
+        public ITripBuilder SetSalesContext()
+        {
+            _trip.SetSalesContext(_salesContext);
+            return this;
+        }
+        public Trip Build()
+        {
+            return _trip;
+        }
+    }
+
+    public class KayakTripBuilder : ITripBuilder
+    {
+        private Trip _trip;
+        private string _description = "到海上划獨木舟";
+        private string _salesContext = "使用振興券參加獨木舟行程 再折 100 元";
+        public KayakTripBuilder()
+        {
+            _trip = new Trip();
+        }
+        public ITripBuilder SetDestination(string destination)
+        {
+            _trip.SetDestination(destination);
+            return this;
+        }
+        public ITripBuilder SetPrice(int price)
+        {
+            _trip.SetPrice(price);
+            return this;
+        }
+        public ITripBuilder SetDifficulty(int difficulty)
+        {
+            _trip.SetDifficulty(difficulty);
+            return this;
+        }
+        public ITripBuilder SetDurationHours(int hours)
+        {
+            _trip.SetDurationHours(hours);
+            return this;
+        }
+        public ITripBuilder SetMaxParticipants(int maxParticipants)
+        {
+            _trip.SetMaxParticipants(maxParticipants);
+            return this;
+        }
+        public ITripBuilder SetDescription()
+        {
+            _trip.SetDescription(_description);
+            return this;
+        }
+        public ITripBuilder SetSalesContext()
+        {
+            _trip.SetSalesContext(_salesContext);
+            return this;
+        }
+        public Trip Build()
+        {
+            return _trip;
+        }
+    }
+    public class Trip
+    {
+        private string _destination;
+        private int _price;
+        private int _difficulty;
+        private int _hours;
+        private int _maxParticipants;
+        private string _description;
+        private string _salesContext;
+        private IList<string> _tripDetail = new List<string>();
+        public IEnumerable<string> GetDetail()
+        {
+            foreach (var item in _tripDetail)
+            {
+                yield return item;
+            }
+        }
+        // Getters to be added
+        public void SetDestination(string destination)
+        {
+            _destination = destination;
+            _tripDetail.Add($"地點: {destination}");
+        }
+        public void SetPrice(int price)
+        {
+            _price = price;
+            _tripDetail.Add($"每人價格: NTD {price}");
+        }
+        public void SetDifficulty(int difficulty)
+        {
+            _difficulty = difficulty;
+            _tripDetail.Add($"困難度: {difficulty}/5");
+        }
+        public void SetDurationHours(int hours)
+        {
+            _hours = hours;
+            _tripDetail.Add($"時間: {hours} 小時");
+        }
+        public void SetMaxParticipants(int maxParticipants)
+        {
+            _maxParticipants = maxParticipants;
+            _tripDetail.Add($"每團人數限制: {maxParticipants} 人");
+        }
+        public void SetDescription(string description)
+        {
+            _description = description;
+            _tripDetail.Add($"SUP 活動敘述: {_description}");
+        }
+        public void SetSalesContext(string salesContext)
+        {
+            _salesContext = salesContext;
+            _tripDetail.Add($"【{_salesContext}】");
+        }
+    }
+
+
+    class Program
+    {
+        static void PrintTripDetail(Trip trip)
+        {
+            var tripDetail = trip.GetDetail();
+            foreach (var item in tripDetail)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("---");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Trip 1. SUP 象鼻岩行程");
+            Trip trip = new SupTripBuilder()
+                        .SetSalesContext()
+                        .SetDestination("深澳象鼻岩")
+                        .SetPrice(2000)
+                        .SetDifficulty(2)
+                        .SetDurationHours(4)
+                        .SetMaxParticipants(15)
+                        .SetDescription()
+                        .Build();
+            PrintTripDetail(trip);
+
+            Console.WriteLine("Trip 2. SUP 龜山島行程");
+            Trip trip1 = new SupTripBuilder()
+                        .SetSalesContext()
+                        .SetDestination("龜山島牛奶湖")
+                        .SetPrice(5000)
+                        .SetDifficulty(4)
+                        .SetDurationHours(6)
+                        .SetMaxParticipants(10)
+                        .SetDescription()
+                        .Build();
+            PrintTripDetail(trip1);
+
+            Console.WriteLine("Trip 3. 獨木舟 小琉球行程");
+            Trip trip2 = new KayakTripBuilder()
+                        .SetSalesContext()
+                        .SetDestination("小琉球")
+                        .SetPrice(3500)
+                        .SetDifficulty(2)
+                        .SetDurationHours(5)
+                        .SetMaxParticipants(20)
+                        .SetDescription()
+                        .Build();
+            PrintTripDetail(trip2);
+
+            Console.WriteLine("Trip 4. 獨木舟 東澳行程");
+            Trip trip3 = new KayakTripBuilder()
+                        .SetSalesContext()
+                        .SetDestination("東澳海蝕洞")
+                        .SetPrice(1500)
+                        .SetDifficulty(3)
+                        .SetDurationHours(4)
+                        .Build();
+            PrintTripDetail(trip3);
+
+            Console.WriteLine("Trip 5. 獨木舟 龍洞行程");
+            Trip trip4 = new KayakTripBuilder()
+                        .SetSalesContext()
+                        .SetDestination("龍洞")
+                        .SetPrice(1500)
+                        .Build();
+            PrintTripDetail(trip4);
+        }
+    }
+}
