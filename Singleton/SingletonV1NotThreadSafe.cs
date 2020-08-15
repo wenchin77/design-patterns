e5a60026a9757ed11c40c6a96d31bda65092930f
Wenchin <wenchin.ch@gmail.com>
2020-08-15 10:03:03 +0800

    Updated Singleton v1's comment

--- Singleton/SingletonV1NotThreadSafe.cs
+++ Singleton/SingletonV1NotThreadSafe.cs
@@ -4,7 +4,6 @@ namespace Singleton
 {
     class SingletonV1NotThreadSafe : NumberGenerator
     {
-        // 用靜態欄位初始化 _instance 並不賦予值 (multithread 時會有問題)
         private static SingletonV1NotThreadSafe _instance = null;
 
         // 建構子設為 private 避免外面可以直接 new 一個物件的情況
